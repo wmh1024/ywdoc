c0a6d508eff8cda4b2dc789b9ca8c518399118c2
wmh <wmh031024@163.com>
2024-04-21 17:39:21 +0800

    vitepress init

---
+++ README.md
@@ -0,0 +1,17 @@
+# what
+
+it is a template for VitePress Site;
+
+it includes some configs out of box, e.g. sidebar and menus;
+
+you don't need waste your time on vitepress config;
+
+## how to use
+
+1、git clone this repo
+
+2、yarn
+
+3、docs:dev
+
+4、write your docs
---
+++ docs/.vitepress/config.js
@@ -0,0 +1,42 @@
+import menu1Sidebar from "./menu1Sidebar";
+import menu2Sidebar from "./menu2Sidebar";
+import menu3Sidebar from "./menu3Sidebar";
+
+export default {
+    base: '/',
+
+    title: '氧化氢', // 所有文档的浏览器标签title
+    description: '氧化氢的网站', // 会渲染成<meta>标签，SEO用
+
+    themeConfig: {
+
+        siteTitle: '氧化氢的网站',
+        logo: '/logo.png',
+
+
+        nav: [
+            { text: '菜单1', link: '/menu1/', activeMatch: '/menu1/' },
+            { text: '菜单2', link: '/menu2/', activeMatch: '/menu2/' },
+            { text: '菜单3', link: '/menu3/', activeMatch: '/menu3/' },
+        ],
+
+        sidebar: {
+            '/menu1/': menu1Sidebar,
+            '/menu2/': menu2Sidebar,
+            '/menu3/': menu3Sidebar,
+        },
+
+
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+            { icon: 'twitter', link: '...' },
+            { icon: 'discord', link: '...' }
+        ],
+
+
+        footer: {
+            message: 'Released under the MIT License.',
+            copyright: 'Copyright © 2022-present feiye'
+        },
+    }
+}
